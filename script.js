e1aa6de3c18f738962c691f2bfd26b85dfc85240
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-03-27 21:45:44 +0200

    done implimenting computer selection in js

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+let computerChoice = '';
+
 // console.log(allGameIcons);
 
 // Reset all 'selected' icons
@@ -35,9 +37,60 @@ function resetSelected() {
   }); 
 }
 
+// Random computer choice
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.random();
+  if(computerChoiceNumber < 0.2) {
+    computerChoice = 'rock';
+  } else if (computerChoiceNumber <= 0.4) {
+    computerChoice = 'paper';
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = 'scissors';
+  } else if (computerChoiceNumber <= 0.8) {
+    computerChoice = 'lizard';
+  } else {
+    computerChoice = 'spock';
+  }
+}
+
+// Add 'selected' styling & computerChoice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
+      break;
+    default:
+      break;
+  }
+}
+
+// Call functions to process turn
+function checkResult() {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+}
+
 // Passing player selection value and styling icons
 function select(playerChoice) {
-  resetSelected();
+  checkResult();
   // Add 'selected' styling & playerChoice
   switch (playerChoice) {
     case 'rock':
@@ -59,6 +112,8 @@ function select(playerChoice) {
     case 'spock':
       playerSpock.classList.add('selected');
       playerChoiceEL.textContent = ' --- Spock';
+      break;
+    default:
       break;
   }
 }
